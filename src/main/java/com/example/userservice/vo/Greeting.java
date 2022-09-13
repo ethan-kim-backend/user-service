538aaee2c1f5a9982ebf8ea694b4cef5c082c2c5
RenewMyLife <kimda0ljj@gmail.com>
2022-09-13 22:30:13 +0900

    commit

--- src/main/java/com/example/userservice/vo/Greeting.java
+++ src/main/java/com/example/userservice/vo/Greeting.java
@@ -1,2 +1,19 @@
-package com.example.userservice.vo;public class Greeting {
+package com.example.userservice.vo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+// @AllArgsConstructor 아규먼트를 다 가지고 있는 생성자
+// @NoArgsConstructor 아무거도 없는 디폴트 생성자
+public class Greeting {
+	@Value("${greeting.message}")
+	private String message;
 }
